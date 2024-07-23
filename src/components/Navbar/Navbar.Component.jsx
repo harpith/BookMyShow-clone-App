157f0abfb3083603f4ac80fcc231c73205b4513c
harpith <harpithrao2004@gmail.com>
2024-07-23 20:44:12 +0530

    done

--- src/components/Navbar/Navbar.Component.jsx
+++ src/components/Navbar/Navbar.Component.jsx
@@ -50,13 +50,19 @@ function NavLg() {
     <>
       <div className="container flex mx-auto px-4 items-center justify-between">
         <div className="flex items-center w-1/2 gap-3">
-          <div className="w-10 h-10">
-            <img
-              src="https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png"
-              alt="logo"
-              className="w-full h-full"
-            />
+        <h3 className="text-white w-6">
+              book
+              </h3> 
+          <div className="w-10 h-10 mr-0">
+              <img
+                src="https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png"
+                alt="logo"
+                className="w-full h-full"
+              />
           </div>
+          <h3 className="text-white">
+              show
+              </h3>
           <div className="w-full flex items-center gap-3 bg-white px-3 py-1 rounded-md">
             <BiSearch />
             <input
